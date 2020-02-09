48c9c91aae2532554019a502bc6fdde541ba3cb4
fabs <faebeee@gmail.com>
2020-02-09 18:02:35 +0100

    Add more loggers

--- src/workers/batch-audit.js
+++ src/workers/batch-audit.js
@@ -2,11 +2,13 @@ import { getSiteConfigById } from '../database/sites';
 import logger from '../logger';
 import { createNewAuditForConfig } from '../utils/create-new-audit';
 
-/**
- * Create audits for a list of site ids
- * @param {string[]} list
- * @return {Promise<void>}
- */
+
+process.on('unhandledRejection', (err) => {
+    logger.error(err);
+    console.log(err);
+    process.exit(1);
+});
+
 process.on('message', async ({ list }) => {
     logger.debug(`Start new batch worker ${ list.length }`);
     for (let i = 0; i < list.length; i++) {
--- src/workers/single-audit.js
+++ src/workers/single-audit.js
@@ -1,6 +1,13 @@
 import logger from '../logger';
 import { createNewAuditForConfig } from '../utils/create-new-audit';
 
+
+process.on('unhandledRejection', (err) => {
+    logger.error(err);
+    console.log(err);
+    process.exit(1);
+});
+
 process.on('message', async ({ config }) => {
     logger.debug(`Start new worker`);
     await createNewAuditForConfig(config);
