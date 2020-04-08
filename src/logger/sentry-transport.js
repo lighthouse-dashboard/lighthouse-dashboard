63900eacadd092569a02a85c2385a65fe1051e5e
fabs <faebeee@gmail.com>
2020-04-08 15:36:24 +0200

    Add details to sentry

--- src/logger/sentry-transport.js
+++ src/logger/sentry-transport.js
@@ -1,11 +1,17 @@
 import * as Sentry from '@sentry/node';
 import { Transport } from 'winston';
+import { version } from '../../package';
 
 export default class SentryTransport extends Transport {
 
     constructor(dsn) {
         super();
-        Sentry.init({ dsn });
+        Sentry.init({
+            dsn: dsn,
+            environment: process.env.IS_WORKER ? 'worker' : 'web',
+            release: version,
+            serverName: process.env.SERVICE_NAME || 'local',
+        });
     }
 
     // eslint-disable-next-line class-methods-use-this
