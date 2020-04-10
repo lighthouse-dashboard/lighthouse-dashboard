9b79f1c3e9d26a08509b448f464c79c7e675aa81
fabs <faebeee@gmail.com>
2020-04-10 13:18:35 +0200

    Update code structure

--- src/api/reports/schemas/report-model.js
+++ src/api/reports/schemas/report-model.js
@@ -1,22 +1,44 @@
 import joi from '@hapi/joi';
 
 export const reportValueModel = joi.object({
-    id: joi.string().required(),
-    value: joi.number().required(),
+    id: joi
+        .string()
+        .required(),
+
+    value: joi
+        .number()
+        .required(),
 }).label('reports.ReportValueModel');
 
 
 export const reportModel = joi.object({
-    siteId: joi.string().required(),
-    createdAt: joi.string().required(),
-    message: joi.string().allow(null),
-    git_commit: joi.string().allow(null),
+    siteId: joi
+        .string()
+        .required(),
+
+    createdAt: joi
+        .string()
+        .required(),
+
+    message: joi
+        .string()
+        .allow(null),
+
+    git_commit: joi
+        .string()
+        .allow(null),
+
     raw: joi
         .object()
         .allow(null)
         .optional(),
-    hasRawData: joi.boolean(),
-    values: joi.array().items(reportValueModel),
+
+    hasRawData: joi
+        .boolean(),
+
+    values: joi
+        .array()
+        .items(reportValueModel),
 }).label('reports.ReportModel');
 
 export const reportModelList = joi
