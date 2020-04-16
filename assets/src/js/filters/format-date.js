46586af11d23e47b674f8458bf920e8741331150
fabs <faebeee@gmail.com>
2020-04-16 21:16:46 +0200

    Fix date formatting

--- assets/src/js/filters/format-date.js
+++ assets/src/js/filters/format-date.js
@@ -1,6 +1,15 @@
 import { format } from 'date-fns';
 import { DATE_FORMAT } from '../../../../config/dashboard';
 
+/**
+ * Format date
+ * @param {string|null} value
+ * @return {string|null}
+ */
 export default function formatDate(value) {
+    if (!value) {
+        return null;
+    }
+
     return format(new Date(value), DATE_FORMAT);
 };
