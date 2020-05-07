00454518db6a5c4e0d741a71a36953518b6a870c
fabs <faebeee@gmail.com>
2020-05-07 07:44:58 +0200

    Fix lint

--- assets/src/js/components/navigation/navigation-toggle/navigation-toggle.vue
+++ assets/src/js/components/navigation/navigation-toggle/navigation-toggle.vue
@@ -1,16 +1,14 @@
 <template>
     <div class='navigation-toggle'>
-            <div class='navigation-toggle--content-wrapper'>
-                <button class='navigation-toggle--toggle-btn'
-                        @click='$emit("toggle")'>
-                    Open
-                </button>
-            </div>
+        <div class='navigation-toggle--content-wrapper'>
+            <button class='navigation-toggle--toggle-btn'
+                    @click='$emit("toggle")'>
+                Open
+            </button>
         </div>
+    </div>
 </template>
 
 <script>
-    export default {
-
-    };
+    export default {};
 </script>
