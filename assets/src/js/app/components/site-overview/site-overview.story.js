421c7173e53b5008707d23bbb30ffa1b93b0bdda
fabs <faebeee@gmail.com>
2020-05-21 11:17:24 +0200

    Fix stroy for site overview

--- assets/src/js/app/components/site-overview/site-overview.story.js
+++ assets/src/js/app/components/site-overview/site-overview.story.js
@@ -1,4 +1,5 @@
 import { text } from '@storybook/addon-knobs';
+import createFakeReport from '../../../../../../src/utils/create-fake-report';
 import SiteOverview from './site-overview.vue';
 
 export default { title: 'Components / Site Overview' };
@@ -12,23 +13,12 @@ export const component = () => ({
     },
     data() {
         return {
-            datasets: [{
-                name: 'performance',
-                data: [100, 100, 92, 99, 98, 95, 97, 100, 98, 85, 97, 78, 99, 100, 0, 0, 93, 99, 99, 99, 99, 100, 100, 100, 98, 98, 97, 100, 99, 100],
-            }, {
-                name: 'seo',
-                data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
-            }, {
-                name: 'pwa',
-                data: [31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 58, 31, 31, 31, 31, 31, 31],
-            }, {
-                name: 'accessibility',
-                data: [90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90],
-            }, {
-                name: 'best-practices',
-                data: [86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86, 86],
-            }],
+            reports: [
+                createFakeReport(),
+                createFakeReport(),
+                createFakeReport(),
+            ],
         };
     },
-    template: '<site-overview :name="name" url="http://fabs.io" id="123" :datasets="datasets" :labels="[\'Foo\']"/>',
+    template: '<site-overview :name="name" url="http://fabs.io" id="123" :reports="reports" />',
 });
---
+++ src/utils/create-fake-report.js
@@ -0,0 +1,37 @@
+/**
+ * Create new faked report
+ * @return {Report}
+ */
+export default function createFakeReport() {
+    return {
+        id: 'foo',
+        siteId: 'foo',
+        createdAt: (new Date()).toString(),
+        message: 'Fake Report',
+        git_commit: 'Fake report',
+        raw: null,
+        hasRawData: false,
+        values: [
+            {
+                id: 'performance',
+                value: Math.round(Math.random() * 100),
+            },
+            {
+                id: 'seo',
+                value: Math.round(Math.random() * 100),
+            },
+            {
+                id: 'accessibility',
+                value: Math.round(Math.random() * 100),
+            },
+            {
+                id: 'best-practices',
+                value: Math.round(Math.random() * 100),
+            },
+            {
+                id: 'pwa',
+                value: Math.round(Math.random() * 100),
+            },
+        ],
+    };
+}
