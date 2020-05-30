29a52b7bb6d7d18e90cd87dfbd76c0fee3fc13a8
fabs <faebeee@gmail.com>
2020-05-30 10:43:50 +0200

    Fix redirect after login

--- assets/src/js/app/components/login-form/login-form.vue
+++ assets/src/js/app/components/login-form/login-form.vue
@@ -70,7 +70,7 @@
                 this.error = null;
                 try {
                     await this.doLogin({ password: this.password });
-                    this.$router.push({ name: 'app' });
+                    this.$router.push({ name: 'dashboard' });
                     this.isLoading = false;
                 } catch (e) {
                     this.error = e;
