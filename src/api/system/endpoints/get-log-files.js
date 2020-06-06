68bea73020e71f908ea5f8a3020f27462bb88ae1
fabs <faebeee@gmail.com>
2020-06-06 23:23:01 +0200

    Add api to get logfiles

---
+++ src/api/system/endpoints/get-log-files.js
@@ -0,0 +1,30 @@
+import joi from '@hapi/joi';
+import glob from 'glob';
+import path from 'path';
+
+function getLogFiles() {
+    return new Promise((resolve, reject) => {
+        glob(path.resolve(__dirname, '../../../../logs/*.log'), {}, (er, files) => {
+            if (er) {
+                return reject(er);
+            }
+
+            const mappedFiles = files.map(f => path.basename(f));
+            return resolve(mappedFiles);
+        });
+    });
+}
+
+export default {
+    method: 'GET',
+    path: '/api/system/logfiles',
+    handler: getLogFiles,
+    options: {
+        description: 'Get logfiles',
+        tags: ['api', 'system'],
+        auth: 'jwt',
+        response: {
+            schema: joi.array().items(joi.string()),
+        },
+    },
+};
