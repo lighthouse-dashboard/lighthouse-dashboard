85a8dce308d5be68fa55430f1f7fcfc0dd5304fa
fabs <faebeee@gmail.com>
2020-06-20 11:28:24 +0200

    Remove servicecontainer

--- src/api/reports/endpoints/get-reports.js
+++ src/api/reports/endpoints/get-reports.js
@@ -1,6 +1,6 @@
 import Boom from '@hapi/boom';
-import ServiceContainer from 'servicecontainer';
 import CONFIG from '../../../../config/server';
+import { getReportsBySiteId } from '../../../services/models/reports';
 import { getSiteConfigById } from '../../../services/models/sites';
 import { reportIdParamModel } from '../schemas/report-id-param-model';
 import { reportModelList } from '../schemas/report-model-schema';
@@ -13,14 +13,13 @@ import { reportModelList } from '../schemas/report-model-schema';
  */
 async function getReports({ params, mongo }) {
     const { id } = params;
-    const reportService = ServiceContainer.get().get('reports');
 
     const config = getSiteConfigById(mongo.db, id);
     if (!config) {
         return Boom.notFound(`Site with id not found`);
     }
 
-    const assets = await reportService.getReportsBySiteId(mongo.db, id, CONFIG.api.siteReportLimit);
+    const assets = await getReportsBySiteId(mongo.db, id, CONFIG.api.siteReportLimit);
     if (!assets || assets.length === 0) {
         return [];
     }
