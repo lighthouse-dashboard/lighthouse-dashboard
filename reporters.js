eea11af79dd4ac2dfc49bed82987eec68598f05f
fabs <faebeee@gmail.com>
2020-09-06 14:03:34 +0200

    Upgrade QM reporter

--- reporters.js
+++ reporters.js
@@ -1,9 +1,9 @@
 import { register } from './lib/reporter';
 import * as lighthouseDashboard from './lib/reporter/integrations/lighthouse-dashboard';
-import qm from './lib/reporter/integrations/quickmetrics';
+import * as qm from './lib/reporter/integrations/quickmetrics';
 
 if (process.env.QUICK_METRICS_KEY) {
-    register(qm(process.env.QUICK_METRICS_KEY));
+    register(qm);
 }
 
 
