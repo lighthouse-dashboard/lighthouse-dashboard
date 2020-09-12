091a2bca1513189b82935c5e56a8159c4eb7ef34
fabs <faebeee@gmail.com>
2020-09-12 20:53:23 +0200

    Fix captions

--- assets/src/js/app/components/site-config/site-config.vue
+++ assets/src/js/app/components/site-config/site-config.vue
@@ -9,6 +9,9 @@
 
         <ul class="list">
             <li class="list-item">
+                <p class="u-reset caption list-item--caption">
+                    {{ $t('site.name-field') }}
+                </p>
                 <a class="u-reset subtitle1 link"
                         :href="site.url">
                     {{ site.name }}
@@ -18,8 +21,8 @@
 
             <li class="list-item"
                     v-if="site.is_disabled">
-                <p class="u-reset caption">
-                    {{ $t('general.last-audit') }}
+                <p class="u-reset caption list-item--caption">
+                    {{ $t('site.is-disabled') }}
                 </p>
                 <p class="u-reset subtitle1 list-item--content">
                     <unicon name="ban"/>
@@ -41,15 +44,21 @@
 
             <li class="list-item"
                     v-if="!site.is_public">
+                <p class="u-reset caption list-item--caption">
+                    {{ $t('site.is-public') }}
+                </p>
                 <p class="u-reset subtitle1 list-item--content">
                     <unicon name="lock"
                             class="list-item--icon"/>
-                    {{ $t('site.is-public') }}
+                    {{ $t('site.private') }}
                 </p>
             </li>
 
             <li class="list-item"
                     v-if="site.is_favorite">
+                <p class="u-reset caption list-item--caption">
+                    {{ $t('site.device') }}
+                </p>
                 <p class="u-reset subtitle1 list-item--content">
                     <unicon name="heart"
                             class="list-item--icon"/>
@@ -58,7 +67,7 @@
             </li>
 
             <li class="list-item">
-                <p class="u-reset caption">
+                <p class="u-reset caption list-item--caption">
                     {{ $t('site.device') }}
                 </p>
                 <p class="u-reset subtitle1 site-config--list-item-value">
